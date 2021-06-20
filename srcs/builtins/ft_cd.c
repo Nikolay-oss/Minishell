8a115cf54d6a46193eaa5960c267ff25314c1591
Galads <nikola_2212@mail.ru>
2021-06-20 03:12:22 +0300

    теперь все работает нормально кроме старого пути, еще необходимо точно на лики посмотреть

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -4,21 +4,32 @@ void	ft_cd(t_list *env, char *path)
 {
 	int res;
 	char *buf;
+	struct s_node *node;
+	struct s_node *node_old;
 
 	if (!path || !strcmp(path, "~"))
-		printf("loool\n");
+	{
+		path = getenv_node(env, "HOME")->content;
+		path = ft_strrchr(path, '=');
+		path++;
+	}
 	res = chdir(path);
+	node = getenv_node(env, "PWD");
 	if(res < 0)
 		printf("Error: %s\n", strerror(errno));
 	else
 	{
-		buf = getcwd(NULL, 0);
-		printf("->|%s|\n", buf);
-//		getenv_node(env, "OLD_PWD")->content
-//				= getenv_node(env, "PWD")->content;
-//		free(getenv_node(env, "PWD")->content);
-		printf("---content: %s", (char *)(getenv_node(env, "PWD")->content));
-		(getenv_node(env, "PWD")->content) = (void *)buf;
+		buf = getcwd(NULL, PATH_MAX);
+//		printf("->|%s|\n", buf);
+		if (node && node->content)
+			free(node->content);
+//		printf("---content: %s\n", (char *)node->content);
+//		node_old = getenv_node(env, "OLDPWD");
+//		printf("lllllllllllllll\n");
+//		if(node_old)
+//			node_old->content = node->content;
+		node->content = (void *)ft_strjoin("PWD=", buf);
 		printf("%s\n", buf);
+		free(buf);
 	}
 }
