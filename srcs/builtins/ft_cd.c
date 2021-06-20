e2ef34e5906a28a1b945e0a1190b9d9c12f43673
Galads <nikola_2212@mail.ru>
2021-06-20 17:44:54 +0300

    базовая обработка согласно сабджекту

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -1,11 +1,25 @@
 #include "minishell.h"
 
+void ft_cd_handler(char **buf, t_node *node, t_node *node_old, t_list *env)
+{
+	(*buf) = getcwd(NULL, PATH_MAX);
+	node_old = getvar_node(env, "OLDPWD");
+	if(node_old)
+	{
+		free(node_old->content);
+		node->content = ft_strrchr((node->content), '=');
+		(node->content)++;
+		node_old->content = (void *)ft_strjoin("OLDPWD=", node->content);
+	}
+	node->content = (void *)ft_strjoin("PWD=", (*buf));
+}
+
 void	ft_cd(t_list *env, char *path)
 {
-	int res;
-	char *buf;
-	struct s_node *node;
-	struct s_node *node_old;
+	int		res;
+	char	*buf;
+	t_node	*node;
+	t_node	*node_old;
 
 	if (!path || !strcmp(path, "~"))
 	{
@@ -16,19 +30,10 @@ void	ft_cd(t_list *env, char *path)
 	res = chdir(path);
 	node = getvar_node(env, "PWD");
 	if(res < 0)
-		printf("Error: %s\n", strerror(errno));
+		printf("cd: %s: %s\n", strerror(errno), path);
 	else
 	{
-		buf = getcwd(NULL, PATH_MAX);
-//		printf("->|%s|\n", buf);
-//		printf("---content: %s\n", (char *)node->content);
-		node_old = getvar_node(env, "OLDPWD");
-//		printf("lllllllllllllll\n");
-		if(node_old)
-			node_old->content = (void *)ft_strdup(node->content);
-		if (node && node->content)
-			free(node->content);
-		node->content = (void *)ft_strjoin("PWD=", buf);
+		ft_cd_handler(&buf, node, node_old, env);
 		printf("%s\n", buf);
 		free(buf);
 	}
