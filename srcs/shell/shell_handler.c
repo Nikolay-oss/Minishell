e9daa91f1dd9bb2da8337edf894eb7a717beabb5
Dipoo Kenchur <dkenchur@am-i1.msk.21-school.ru>
2021-07-13 23:12:44 +0300

    fix process counter

--- srcs/shell/shell_handler.c
+++ srcs/shell/shell_handler.c
@@ -3,7 +3,7 @@
 void	shell_handler(t_minishell *minishell)
 {
 	heredocs_handler(minishell);
-	minishell->cnt_prcs = 0;
+	minishell->cnt_prcs = -1;
 	if (minishell->pipes_count > 0)
 	{
 		ft_pipes(minishell, minishell->commands, 0);
